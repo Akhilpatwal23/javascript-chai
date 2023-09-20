8324c8093b610bbd8c0b5aade7e3d2ad4f0f0c74
Akhilpatwal23 <75497769+Akhilpatwal23@users.noreply.github.com>
2023-09-20 15:17:50 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = "33avb";
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score); 
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+
+let bol=1;
+let check=Boolean(bol);
+// console.log(check);
+// 1=> true; 0=> false;
+// ""=> flase
+// "akhil"=> true
+
+let someNumber = 33
+ let stringNumber = String(someNumber);
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
