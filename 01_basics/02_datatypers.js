5416d04b6ae6d28891ec43df1b25fde46437762f
Akhilpatwal23 <75497769+Akhilpatwal23@users.noreply.github.com>
2023-09-20 04:27:04 +0000

    datatypes

---
+++ 01_basics/02_datatypers.js
@@ -0,0 +1,14 @@
+// use strict / treat all js code as newer version
+"use strict"; 
+//aleart("hello");//we are  using node js 
+console.log(3+4,"hello");// code readability should be high
+
+//num => 2 to the pow of 52
+// bigint
+//string => "hello"
+//boolean => true / false
+// null = > standalone value
+// undefined =>
+//symbol => unique
+
+//object
